0269c62fa76f5a91e0f2c8679bdde8d3d7d80d48
Anderson de Medeiros <a3medeiros@gmail.com>
2018-05-01 01:19:03 -0300

    Refactored code

--- app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -1,114 +1,89 @@
 package com.mytaxi.android_demo;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.*;
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import com.mytaxi.android_demo.activities.MainActivity;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-
 public class ExampleInstrumentedTest {
+
+    private static final String USERNAME = "whiteelephant261";
+    private static final String PASSWORD = "video1";
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-   /* @Test
+    @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
     }
-*/
+
+    public void clickWithIdAndTypeText(int id, String txt ){
+       ViewInteraction typeField = onView(withId(id)).perform(click());
+       typeField.perform(typeText(txt), closeSoftKeyboard());
+    }
+
+    public void clickButtonWithId(int id) {
+        ViewInteraction loginBtn = onView(withId(id));
+        this.clickS(loginBtn);
+    }
+
+    public void selectDriverByName(String driverName){
+        ViewInteraction fieldTaxiName = onView(withText(driverName)).inRoot(isPlatformPopup()).check(matches(isDisplayed()));
+        this.clickS(fieldTaxiName);
+    }
+
+    public void clickS(ViewInteraction view){
+       view.perform(click());
+    }
+
     @Test
     public void testLoginSearchAndCall() throws InterruptedException {
-        String username = "whiteelephant261";
 
         Thread.sleep(5000);
 
         //Username field and type text
-        ViewInteraction fieldUsername = onView(withId(R.id.edt_username)).perform(click());
-        fieldUsername.perform(typeText(username), closeSoftKeyboard());
+        this.clickWithIdAndTypeText(R.id.edt_username,USERNAME);
 
         //Password field and type text
-        ViewInteraction fieldPswd = onView(withId(R.id.edt_password));
-        fieldPswd.perform(typeText("video1"), closeSoftKeyboard());
+        this.clickWithIdAndTypeText(R.id.edt_password,PASSWORD);
 
         //Find login button and click it
-        ViewInteraction loginBtn = onView(allOf(withId(R.id.btn_login), withText("Login")));
-        loginBtn.perform(click());
+        this.clickButtonWithId(R.id.btn_login);
 
         Thread.sleep(5000);
 
         //Taxi part
         //Find search field and text "sa"
-        ViewInteraction fieldSearch = onView(withId(R.id.textSearch));
-        fieldSearch.perform(typeText("sa"), closeSoftKeyboard());
+        this.clickWithIdAndTypeText(R.id.textSearch,"sa");
 
         Thread.sleep(3000);
-
-        //Find in the screen and click the taxi with name "Sarah Friedrich"
-        ViewInteraction fieldTaxiName = onView(withText("Sarah Friedrich")).inRoot(isPlatformPopup()).check(matches(isDisplayed()));
-        fieldTaxiName.perform(click());
+        this.selectDriverByName("Sarah Friedrich");
 
         //Click call button
-        ViewInteraction btnCall = onView(withId(R.id.fab));
-        btnCall.perform(click());
+        this.clickButtonWithId(R.id.fab);
 
         Thread.sleep(3000);
+
         //Validation of test
         Context appContext = InstrumentationRegistry.getContext();
         assertNotEquals("com.mytaxi.android_demo", appContext.getPackageName());
-    }
-
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
